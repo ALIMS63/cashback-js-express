bebc4f8b47f5bfa2bfc52bbe1f9d755bbe12d067
Pavel <pavtar676@gmail.com>
2020-08-27 11:27:32 +0300

    Authorization v1.0

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/users')
 
+router.use((req, res, next) => {
+  if (req.session.invalidpass) {
+    res.locals.invalidpass = true;
+  } else {
+    res.locals.invalidpass = false;
+  }
+  next()
+})
+
 router.get('/', (req, res) => {
   res.render('authorization');
 });
@@ -9,12 +18,15 @@ router.get('/', (req, res) => {
 router.post('/api', async (req, res) => {
   const person = await User.find({ number: req.body.number, password: req.body.password })
   if (person.length != 0) {
+    req.session.invalidpass = false
     console.log(person[0].number)
     console.log(req.session.user)
     req.session.user = person[0].number
     console.log(req.session.user)
     res.redirect('/admin')
   }
+  req.session.invalidpass = true
+  console.log(req.session.invalidpass)
   res.redirect('/')
 })
 
